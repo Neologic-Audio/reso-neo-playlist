3f0c0c1cd03eeb13c6493be54e79a998719b982d
gwhite2019 <gwhite2019@fau.edu>
2022-04-24 22:32:33 -0400

    fix: player

--- playlist/static/js/search.js
+++ playlist/static/js/search.js
@@ -1,10 +1,10 @@
 /* Description: Custom JS for Search Functions */
 $(function () {
-    // function showTrack(released) {
-    //     document.getElementById("player").src = "https://stream.resonate.coop/embed/track/" + released;
-    //     document.getElementById("listen").href = "https://stream.resonate.coop/track/" + released;
-    //
-    // }
+    function showTrack(released) {
+        document.getElementById("player").src = "https://stream.resonate.coop/embed/track/" + released;
+        document.getElementById("listen").href = "https://stream.resonate.coop/track/" + released;
+
+    }
 
     function search() {
         const query = $("#search").find("input[name=search]").val();
@@ -50,25 +50,25 @@ $(function () {
     $("#search").submit(search1);
     search1();
 
-    function searchPlaylist() {
-        const query = $("#search").find("input[name=search]").val();
-        $.get("/search_playlists?q=" + encodeURIComponent(query),
-            function (data) {
-                const t = $("table#playlist tbody").empty();
-                if (!data || data.length == 0) return;
-                data.forEach(function (track) {
-                    $("<tr><td class='track'>" + track.title + "</td><td>" + track.tagline + "</td></tr>"
-                    ).appendTo(t)
-                        .click(function () {
-                            showTrack(track.released);
-
-                        })
-
-                });
-                showTrack(data[0].released);
-            }, "json");
-        return false;
-    }
+    // function searchPlaylist() {
+    //     const query = $("#search").find("input[name=search]").val();
+    //     $.get("/search_playlists?q=" + encodeURIComponent(query),
+    //         function (data) {
+    //             const t = $("table#playlist tbody").empty();
+    //             if (!data || data.length == 0) return;
+    //             data.forEach(function (track) {
+    //                 $("<tr><td class='track'>" + track.title + "</td><td>" + track.tagline + "</td></tr>"
+    //                 ).appendTo(t)
+    //                     .click(function () {
+    //                         showTrack(track.released);
+    //
+    //                     })
+    //
+    //             });
+    //             showTrack(data[0].released);
+    //         }, "json");
+    //     return false;
+    // }
 
     $("#search").submit(searchPlaylist);
     searchPlaylist();
